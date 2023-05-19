5f89ba0055a4c1220f30378f35d6766f1e002a40
Gabriel Penman <gabrielpenman@gmail.com>
2023-05-19 05:01:46 +0100

    Adding functionality to the server

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,  request, jsonify
 
 try:
     from flask_pymongo import PyMongo
@@ -15,4 +15,19 @@ mongo.init_app(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Flask server running!'
+
+@app.route('/upload-svg', methods=['POST'])
+def upload_svg():
+    svg_file = request.files['file']  # Assumes the file is uploaded with the key 'file'
+    svg_data = svg_file.read()  # Read the SVG file data
+    mongo.db.svg_collection.insert_one({'svg_data': svg_data})  # Store the SVG data in MongoDB
+    return 'SVG file uploaded successfully'
+
+@app.route('/get-svg/<svg_id>', methods=['GET'])
+def get_svg(svg_id):
+    svg = mongo.db.svg_collection.find_one({'_id': svg_id})
+    if svg:
+        return jsonify(svg['svg_data'])
+    else:
+        return 'SVG not found', 404
