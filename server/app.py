8e3fa6f964dfb68dbac3313b116dc8557112f7c7
Gabriel Penman <gabrielpenman@gmail.com>
2023-05-19 04:28:15 +0100

    I think the flask server can run now

--- server/app.py
+++ server/app.py
@@ -1,9 +1,17 @@
 from flask import Flask
-from flask_pymongo import PyMongo
+
+try:
+    from flask_pymongo import PyMongo
+except ImportError:
+    import os
+    os.system("pip install Flask-PyMongo")
+    from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase" # Replace with your MongoDB connection string
-mongo = PyMongo(app)
+mongo = PyMongo()
+mongo.init_app(app)
 
 @app.route('/')
 def hello_world():
